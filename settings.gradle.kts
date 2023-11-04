aa4fb6c2c47dc1f92485262fce399ca8532e3145
rouabouassida <146998316+rouabouassida@users.noreply.github.com>
2023-11-04 14:58:22 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mesure_Glycemie_ma_version"
+include(":app")
+ 
